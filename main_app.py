68a503242202eeecbf03ee0159bdc4e4d58f6705
BSSupaschai Ph <112678332+SupaschaiPh@users.noreply.github.com>
2022-11-05 18:19:51 +0700

    เพิ่มการconfig

--- main_app.py
+++ main_app.py
@@ -1,20 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 import uvicorn
+import json
 
 app = FastAPI()
 #หาpathปัจจุบันของโปรเจค
 cur_path_of_py_file = __file__[:__file__.rfind("\\")].replace("\\", "/")
-
+config = json.loads(open(cur_path_of_py_file+"/data/config.json").read())
+tem_path = config["templates path"] if config["templates path"] != "" else cur_path_of_py_file
 
 @app.get("/")
 def home():
-    index_html = open(cur_path_of_py_file+"/templates/home.html").read()
+    index_html = open(tem_path+config["home path"]).read()
     return HTMLResponse(index_html.replace("/*text*/","Hello"))
 
 @app.get("/search")
 def search(keyword:str):
-    index_html = open(cur_path_of_py_file+"/templates/home.html").read()
+    index_html = open(tem_path+config["home path"]).read()
     return HTMLResponse(index_html.replace("/*text*/",keyword))
 
-uvicorn.run(app)
+uvicorn.run(app,port=config["port"])
