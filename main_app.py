63123d91249b308759e1bcf0d8d66bf0a6d53dd5
BSSupaschai Ph <112678332+SupaschaiPh@users.noreply.github.com>
2022-11-05 17:55:01 +0700

    เพิ่มการใช้งานเทมเพลต html

--- main_app.py
+++ main_app.py
@@ -3,13 +3,18 @@ from fastapi.responses import HTMLResponse
 import uvicorn
 
 app = FastAPI()
+#หาpathปัจจุบันของโปรเจค
+cur_path_of_py_file = __file__[:__file__.rfind("\\")].replace("\\", "/")
+
 
 @app.get("/")
-def index():
-    return HTMLResponse("<h1>sawasdeejaaaa</h1>")
+def home():
+    index_html = open(cur_path_of_py_file+"/templates/home.html").read()
+    return HTMLResponse(index_html.replace("/*text*/","Hello"))
 
-@app.get("/567")
-def index():
-    return HTMLResponse("<h1>ฟหกด</h1>")
+@app.get("/search")
+def search(keyword:str):
+    index_html = open(cur_path_of_py_file+"/templates/home.html").read()
+    return HTMLResponse(index_html.replace("/*text*/",keyword))
 
 uvicorn.run(app)
