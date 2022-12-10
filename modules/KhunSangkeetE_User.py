163dabcf101f62d9f7c76fba0b3b1c947f0601b7
BosSupaschai Ph <112678332+SupaschaiPh@users.noreply.github.com>
2022-12-10 13:51:47 +0700

    เพิ่มโครงระบบ ผู้ใช้

--- modules/KhunSangkeetE_User.py
+++ modules/KhunSangkeetE_User.py
@@ -1,4 +1,4 @@
-from fastapi import  Form, Cookie, UploadFile
+from fastapi import  Form, UploadFile, Cookie
 from fastapi.responses import HTMLResponse, FileResponse
 import pandas as pd
 import json
@@ -13,21 +13,42 @@ genToken = None
 def userSys():
     global app
     @app.get("/")
-    async def home():
+    async def home(adminToken=Cookie("")):
         """หน้าแรกของเว็บ"""
         #ให้ Pyton เอา ข้อมูลเสียง(.json) และ ไฟล์เสียงมาแสดง
-        # ทำอันนี้
-        soundData = json.loads(open(parent_path+config["local_storage"]["sound data"], "r").read())
-        audioTag = ""
-        for _id in soundData:
-            audioTag += f"<audio controls src='/stream/sound/{_id}'></audio>"
-        return HTMLResponse(render_templates(path=parent_path+"/templates/"+config["template"]["home"], data={"text":audioTag}))
+        try:
+            if config["local_storage"]["on"] == 1:
+                soundData = json.loads(open(parent_path+config["local_storage"]["sound data"], "r").read())
+                return HTMLResponse(render_templates(
+                    path=parent_path+"/templates/"+config["template"]["home"], 
+                    data={
+                        "soundData":soundData
+                        }))
+            soundData = pd.read_csv(config["with_gform_and_gsheet"]["csv_link"])
+            soundData = soundData.to_dict("index")
+            #https://drive.google.com/uc?export=download&id=
+            return HTMLResponse(render_templates(
+                    path=parent_path+"/templates/"+config["template"]["home"], 
+                    data={
+                        "soundData":soundData
+                        }))
+        except:
+            return HTMLResponse(render_templates(
+                path=parent_path+"/templates/"+config["template"]["home"], 
+                    data={
+                        "keyword":"มีบางอย่างผิดพลาดโปรดติดต่อผู้ดูแล"
+                        }))
 
     @app.get("/add_sound")
-    async def add_sound():
+    async def add_sound(adminToken=Cookie("")):
         # แสดง หน้าเว็บสำหรับเพิ่มไฟล์
         if config["local_storage"]["on"] == 1:
-            return HTMLResponse(render_templates(path=parent_path+"/templates/"+config["template"]["add_sound"], data={"id":genToken("id")}))
+            return HTMLResponse(render_templates(
+                path=parent_path+"/templates/"+config["template"]["add_sound"], 
+                data={
+                    "id":genToken("id"),
+                    "call":("showAdminBt();" if adminToken != "" else "")
+                    }))
         return HTMLResponse(redirect(config["with_gform_and_gsheet"]["form_link"]))
     @app.post("/add_sound")
     async def save_sent_add_sound(
@@ -36,49 +57,99 @@ def userSys():
         soundFile:UploadFile | None = None,
         description = Form("")
         ):
-        if config["local_storage"]["on"] == 0:
-            return HTMLResponse(redirect(config["with_gform_and_gsheet"]["form_link"]))
-        try:
-            soundData = json.loads(open(parent_path+config["local_storage"]["sound data"], "r").read())
-        except:
-            soundData = dict()
-        soundData_W = open(parent_path+config["local_storage"]["sound data"], "w")
-        soundData[_id]={
-                "name":soundName,
-                "description":description
-            }
-        soundData_W.write(json.dumps(soundData))
-        soundData_W.close()
-        file1 = open(f"{parent_path}{config['local_storage']['sound path']}/{_id}.mp3", "xb")
-        file1.write(soundFile.file.read())
-        file1.close()
-        return HTMLResponse(redirect("/"))
-
-    @app.get("/search")
-    async def search(keyword: str = ""):
-        """หน้าสำหรับแสดงผลค้นหา"""
-        return HTMLResponse(render_templates(path=parent_path+"/templates/"+config["template"]["home"], data={"text":"Hello"}))
-
-    @app.get("/info/{id}")
-    async def show_info_sound(id:str = ""):
-        """หน้าแสดงข้อมูลเสียง"""
-        #แสดงข้อฒุลเสียง
-        return HTMLResponse(render_templates(path = parent_path+"/templates/"+config["template"]["info"]))
-
-    @app.get("/stream/sound/{id}")
-    async def stream_sound(id: str = ""):
-        """สตรีมไฟล์เสียง"""
-        # Use in both admin and user
         if config["local_storage"]["on"] == 1:
             try:
-                open(parent_path+config["local_storage"]["sound path"]+"/"+id+".mp3", "r")
-                return FileResponse(parent_path+config["local_storage"]["sound path"]+"/"+id+".mp3")
+                soundData = json.loads(open(parent_path+config["local_storage"]["sound data"], "r", encoding="utf-8").read())
             except:
-                return HTMLResponse("Something Went Wrong Plase Check Path Or File Are Exist")
+                soundData = dict()
+            soundData_W = open(parent_path+config["local_storage"]["sound data"], "w", encoding="utf-8")
+            soundData[_id]={
+                    "name":soundName,
+                    "description":description
+                }
+            soundData_W.write(json.dumps(soundData))
+            soundData_W.close()
+            file1 = open(f"{parent_path}{config['local_storage']['sound path']}/{_id}.mp3", "xb")
+            file1.write(soundFile.file.read())
+            file1.close()
+            return HTMLResponse(redirect(f"/info/{_id}"))
+        return HTMLResponse(redirect(config["with_gform_and_gsheet"]["form_link"]))
+
+    @app.get("/search")
+    async def search(keyword: str = "",adminToken=Cookie("")):
+        """หน้าสำหรับแสดงผลค้นหา"""
+        try:
+            if keyword == "":
+                return HTMLResponse(redirect("/"))
+            if config["local_storage"]["on"] == 1:
+                soundData = json.loads(open(parent_path+config["local_storage"]["sound data"], "r", encoding="utf-8").read())
+                result = [data for data in soundData if keyword.upper() in soundData[data]["name"].upper() or keyword.upper() in soundData[data]["description"].upper()]
+                return HTMLResponse(render_templates(
+                    path=parent_path+"/templates/"+config["template"]["home"], 
+                    data={
+                        "soundData":soundData,
+                        }
+                ))
+            soundData = pd.read_csv(config["with_gform_and_gsheet"]["csv_link"])
+            soundData = soundData.to_dict("index")
+            # https://drive.google.com/uc?export=download&id=1W6HHUsc6pGZ4BWwWPemAiX9AJryfobRV
+            result = [data for data in soundData if keyword.upper() in soundData[data]["Sound Name"].upper() or keyword.upper() in soundData[data]["Description"].upper()]
+            return HTMLResponse(render_templates(
+                    path=parent_path+"/templates/"+config["template"]["home"], 
+                    data={
+                        "soundData":soundData
+                        }))
+        except:
+            return HTMLResponse(redirect("/"))
+    @app.get("/info/{_id}")
+    async def show_info_sound(_id:str = "",adminToken=Cookie("")):
+        """หน้าแสดงข้อมูลเสียง"""
+        #แสดงข้อฒุลเสียง
+        try:
+            if config["local_storage"]["on"] == 1:
+                soundData = json.loads(open(parent_path+config["local_storage"]["sound data"], "r", encoding="utf-8").read())
+                return HTMLResponse(render_templates(
+                    path = parent_path+"/templates/"+config["template"]["info"],
+                    data = {
+                        "id":_id,
+                        "soundName":soundData[_id]["name"],
+                        "description":soundData[_id]["description"],
+                        "link":"/stream/sound/"+_id
+                    }
+                    ))
+            soundData = pd.read_csv(config["with_gform_and_gsheet"]["csv_link"])
+            soundData = soundData.to_dict("index")
+            return HTMLResponse(render_templates(
+                path = parent_path+"/templates/"+config["template"]["info"],
+                data = {
+                    "id":_id,
+                    "soundName":soundData[int(_id)]["Sound Name"],
+                    "description":soundData[int(_id)]["Description"],
+                    "link":"https://drive.google.com/uc?export=download&id="+soundData[int(_id)]["Sound File"].split("?id=")[1]
+                
+                }
+                ))
+        except:
+            return HTMLResponse(redirect("/"))
+        
+
+    @app.get("/stream/sound/{_id}")
+    async def stream_sound(_id: str = ""):
+        """สตรีมไฟล์เสียง"""
+        # Use in both
+        if config["local_storage"]["on"] == 1:
+            try:
+                open(parent_path+config["local_storage"]["sound path"]+"/"+_id+".mp3", "r")
+                return FileResponse(parent_path+config["local_storage"]["sound path"]+"/"+_id+".mp3")
+            except:
+                return HTMLResponse("มีบางผิดตกปิโปรดตรวจสอบว่าไฟล์มีอยู่หรือไม่!?")
         return HTMLResponse(redirect("/"))
 
     @app.exception_handler(404)
     async def handler_error(req, exc):
         """แสดงหน้า 404 Error"""
-        # Use in both admin and user
-        return HTMLResponse(render_templates(path=parent_path+"/templates/"+config["template"]["err404"]))
+        # Use in both
+        try:
+            return HTMLResponse(render_templates(path=parent_path+"/templates/"+config["template"]["err404"]))
+        except:
+            return "404 File Not Found หาไฟล์ไม่เจออออออออออออออออออออออ"
