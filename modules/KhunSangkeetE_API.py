19df3122c034704595a0d16a5e9815b069768602
namfon00 <112681586+namfon00@users.noreply.github.com>
2022-12-12 19:47:03 +0700

    แก้bug apiไม่แสดงข้อมูลในโหมดGsheet

--- modules/KhunSangkeetE_API.py
+++ modules/KhunSangkeetE_API.py
@@ -15,17 +15,15 @@ def APISys():
                 soundData = json.loads(open(parent_path+config["local_storage"]["sound data"], "r", encoding="utf-8").read())
                 if keyword != "":
                     for _id in soundData:
-                        if (not keyword.upper() in soundData[_id]["name"].upper())\
-                        and (not keyword.upper() in soundData[_id]["description"].upper()):
+                        if keyword.upper() in soundData[_id]["name"].upper():
                             result[_id] = soundData[_id]
-                return JSONResponse(soundData, status_code=200)
-            soundData = pd.read_csv(config["with_gform_and_gsheet"]["csv_link"])
-            soundData = soundData.to_dict("index")
-            if keyword != "":
-                for _id in soundData:
-                    if (not keyword.upper() in soundData[_id]["Sound Name"])\
-                    and (not keyword.upper() in soundData[_id]["Description"]):
-                        result[_id] = soundData[_id]
-            return JSONResponse(result, status_code=200)
+            else:
+                soundData = pd.read_csv(config["with_gform_and_gsheet"]["csv_link"])
+                soundData = soundData.to_dict("index")
+                if keyword != "":
+                    for _id in soundData:
+                        if keyword.upper() in soundData[_id]["Sound Name"]:
+                            result[_id] = soundData[_id]
+            return JSONResponse(result if keyword!="" else soundData, status_code=200)
         except:
             return Response("404 Not Found", status_code=404)
